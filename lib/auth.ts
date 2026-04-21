309f970f26e1f3bbec7db1698bb57c19d849506a
Nikhil <kumarnikhil48578@gmail.com>
2026-04-21 18:39:42 +0530

    feat:setup sendResetPassword email

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,10 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { nextCookies } from "better-auth/next-js";
 import { schema } from "@/db/schema/auth-schema";
+import { Resend } from "resend";
+import ForgotPasswordEmail from "@/components/emails/reset-password";
+
+const resend = new Resend(process.env.RESENDA_API_KEY as string)
 
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL,
@@ -18,6 +22,18 @@ export const auth = betterAuth({
 
     emailAndPassword: {
         enabled: true,
+        sendResetPassword: async ({ user, url }) => {
+            await resend.emails.send({
+                from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+                to: user.email,
+                subject: "Reset your password",
+                react: ForgotPasswordEmail({
+                    username: user.name,
+                    resetUrl: url,
+                    userEmail: user.email,
+                }),
+            });
+        },
     },
     socialProviders: {
         google: {
